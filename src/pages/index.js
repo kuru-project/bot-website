895340fc98970a7dec07e287ac4803e1ea780595
BosEriko <bos.eriko@gmail.com>
2019-01-31 08:43:26 +0800

    index WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,11 +20,13 @@ const IndexPage = () => (
       <Layout>
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
         <div className="text-center mb-5 sm:p-14 md:p-20 lg:p-26 xl:p-32">
-          <h1 className="p-0 pb-3 m-0 font-roboto-condensed">Kuru Anime Bot</h1>
-          <p className="p-0 pb-3 m-0">An all purpose bot for Discord servers!</p>
           <div>
-            <a href="http://google.com" target="_blank" className="no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded">Invite {data.site.siteMetadata.companyName} Bot</a>
-            <a href="http://google.com" target="_blank" className="no-underline hover:bg-grey-lighter bg-white py-1 px-3 text-sm text-kuru rounded">Join our Discord</a>
+            <h1 className="p-0 pb-3 m-0 font-roboto-condensed">Kuru Anime Bot</h1>
+            <p className="p-0 pb-3 m-0">An all purpose bot for Discord servers!</p>
+            <div>
+              <a href="http://google.com" target="_blank" className="inline-block m-3 no-underline hover:bg-grey-lighter bg-white p-3 text-sm text-kuru rounded"><i className="fas fa-robot"></i><span className="ml-2">Invite {data.site.siteMetadata.companyName} Bot</span></a>
+              <a href="http://google.com" target="_blank" className="inline-block m-3 no-underline hover:bg-grey-lighter bg-white p-3 text-sm text-kuru rounded"><i className="fas fa-robot"></i><span className="ml-2">Join our Discord</span></a>
+            </div>
           </div>
         </div>
         <div className="container mx-auto">
